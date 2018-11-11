5ff0a754286ca872e4621b0006db61dbc33d8248
Zsolt <zsoltxyz@gmail.com>
2018-11-11 17:04:45 -0500

    added package stuff to mix.exs

--- mix.exs
+++ mix.exs
@@ -9,6 +9,8 @@ defmodule Exred.Node.GrpcServer.MixProject do
       version: "0.1.1",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
+      description: @description,
+      package: package(),
       deps: deps()
     ]
   end
